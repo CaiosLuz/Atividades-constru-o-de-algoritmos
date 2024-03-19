6f39dd0614c9ac0b630a6073f38a8d7e9d6eea1a
Caio José <caiojsdl16@gmail.com>
2024-03-18 21:36:37 -0300

    AULA 4

--- aual3.java
+++
@@ -1,3 +0,0 @@
-public class aual3 {
-    
-}
---
+++ aula4/Exemplo.java
@@ -0,0 +1,10 @@
+public class Exemplo {
+
+    public static void main(String[] args) {
+
+        // println serve para imprimir uma nova linha
+        System.out.println("Hello word"); 
+
+    }
+    
+}
